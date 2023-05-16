909d65d2c86b1a49363000d7d1522be0bf55c468
SagynSuerkulov <132682444+SagynSuerkulov@users.noreply.github.com>
2023-05-16 20:36:41 +0600

    Add files via upload

---
+++ home_work_.dart
@@ -0,0 +1,42 @@
+// import 'dart:io';
+
+// void main() {
+//   List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+//   List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+//   List result = [];
+//   for (int i = 0; i < a.length; i++) {
+//     bool isDublicate = false;
+//     for (int j = 0; j < b.length; j++) {
+//       if (a[i] == b[j]) {
+//         isDublicate = true;
+//       }
+//     }
+//     if (isDublicate == true && !result.contains(a[i])) {
+//       result.add(a[i]);
+//     }
+//   }
+//   print(result);
+// }
+
+// void main() {
+//   List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   List b = [];
+//   for (int i = 0; i < a.length; i++) {
+//     if (a[i] % 2 == 0) {
+//       b.add(a[i]);
+//     }
+//   }
+//   print(b);
+// }
+
+// void main() {
+//   String a = 'a';
+//   String b = 'dart';
+//   int count = 0;
+//   for (int i = 0; i < b.length; i++) {
+//     if (b[i] == a[0]) {
+//       count++;
+//     }
+//   }
+//   print(count);
+// }
